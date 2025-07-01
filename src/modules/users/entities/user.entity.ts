1a1871fc13eb43fddfc87603dd9715699b2e328d
Minh Nguyen <nguyenminhnguyen1324@gmail.com>
2025-07-01 16:00:18 +0700

    Create users, roles, reports, measure_survey, timeline, victim_interview entity

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,13 +1,36 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, Column, PrimaryColumn, OneToOne, JoinColumn } from "typeorm";
+import { Role } from "@/modules/roles/entities/role.entity";
 
-@Entity("users")
+@Entity('users')
 export class User {
-  @PrimaryGeneratedColumn()
-  id!: number;
+  @PrimaryColumn()
+  username!: string;
 
   @Column()
-  name!: string;
+  password_hash!: string;
 
-  @Column({ unique: true })
+  @Column()
+  fullname!: string;
+
+  @Column({ nullable: true })
+  avatar_url?: string;
+
+  @Column()
   email!: string;
+
+  @Column()
+  phonenumber!: string;
+
+  @Column()
+  create_at!: Date;
+
+  @Column()
+  role_id!: string;
+
+  @OneToOne(() => Role)
+  @JoinColumn({ name: 'role_id' })
+  role!: Role;
+
+  @Column({ type: 'boolean', default: false })
+  is_deleted!: boolean;
 }
