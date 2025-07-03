a1511be6750da9dccfb7b676a7af7b8df16f7da7
DucTin <nguyenductin268@gmail.com>
2025-07-03 09:14:05 +0700

    clean to pull

--- src/test.ts
+++
@@ -1,6 +0,0 @@
-let number: number[]=[];
-
-number.push(1,2,3,4,5);
-// node --loader ts-node/esm src/test.ts node --loader ts-node/esm src/test.ts
-console.log(number);
-// node --loader ts-node/esm src/test.ts
