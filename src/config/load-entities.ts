a0e9d189b5918fd9d308750431cb1d741d3baf66
DucTin <nguyenductin268@gmail.com>
2025-07-07 13:38:42 +0700

    delete tracks

--- src/config/load-entities.ts
+++ src/config/load-entities.ts
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-// import entity to use in config-database file
-
-import { Arrest } from "@/modules/arrests/entities/arrest.entity";
-import { Case } from "@/modules/cases/entities/case.entity";
-import { CasesEvidences } from "@/modules/cases_evidences/entities/cases_evidences.entity";
-import { CaseResult } from "@/modules/cases_results/entities/case_result.entity";
-import { DigitalInvert } from "@/modules/digitals_invests/entities/digital_invest.entity";
-import { Evidence } from "@/modules/evidences/entities/evidence.entity";
-import { EvidencesSuspects } from "@/modules/evidences_suspects/entities/evidences_suspects.entity";
-import { FinancialInvest } from "@/modules/financials_invests/entities/financial_invest.entities";
-import { ForensicInvest } from "@/modules/forensics_invests/entities/forensic_invest.entity";
-import { Indictment } from "@/modules/indictments/entities/indictment.entity";
-import { Interview } from "@/modules/interviews/entities/interview.entity";
-import { InvestigationPlan } from "@/modules/investigations_plans/entities/investigation_plan.entity";
-import { MeasureSurvey } from "@/modules/measures_surveys/entities/measure_survey.entity";
-import { Permission } from "@/modules/permissions/entities/permission.entity";
-import { RolesPermissions } from "@/modules/permissions_roles/entities/permission_role.entity";
-import { PhysicalInvest } from "@/modules/physicals_invests/entities/physical_invest.entity";
-import { Prosecution } from "@/modules/prosecutions/entities/prosecution.entity";
-import { ProsecutionsUsers } from "@/modules/prosecutions_users/entities/prosecutions_users.entity";
-import { Question } from "@/modules/questions/entity/question.entity";
-import { RecordInfo } from "@/modules/records_infors/entities/record_infor.entity";
-import { Report } from "@/modules/reports/entities/report.entity";
-import { ReportsVictims } from "@/modules/reports_victims/entities/reports_victims.entity";
-import { ReportsWitnesses } from "@/modules/reports_witnesses/entities/reports_witnesses.entity";
-=======
 import { Arrest } from "@/modules/arrests/entities/arrest.entity";
 import { CaseResult } from "@/modules/case_results/entities/case_result.entity";
 import { Case } from "@/modules/cases/entities/case.entity";
@@ -51,33 +24,11 @@ import { RecordInfo } from "@/modules/records_infos/entities/record_info.entity"
 import { Report } from "@/modules/reports/entities/report.entity";
 import { ReportVictim } from "@/modules/reports_victims/entities/report_victim.entity";
 import { ReportWitness } from "@/modules/reports_witnesses/entities/report_witness.entity";
->>>>>>> 8bce4497bf846259418229be8f90687e5b0c1b99
 import { Role } from "@/modules/roles/entities/role.entity";
 import { Sentence } from "@/modules/sentences/entities/sentence.entity";
 import { Suspect } from "@/modules/suspects/entities/suspect.entity";
 import { Timeline } from "@/modules/timelines/entities/timeline.entity";
 import { User } from "@/modules/users/entities/user.entity";
-<<<<<<< HEAD
-import { UsersCases } from "@/modules/users_cases/entities/user_case.entity";
-import { Victim } from "@/modules/victims/entities/victim.entity";
-import { VictimInterview } from "@/modules/victims_interviews/entities/victim_interview.entity";
-import { Warrant } from "@/modules/warrants/entities/warrant.entity";
-import { WarrantResult } from "@/modules/warrants_results/entities/warrant_result.entity";
-import { Witness } from "@/modules/witnesses/entities/witness.entity";
-import { WitnessesInterviews } from "@/modules/witnesses_interviews/entities/witnesses_interviews.entity";
-
-export const entities = [
-  Arrest,
-  Case,
-  CaseResult,
-  CasesEvidences,
-  DigitalInvert,
-  Evidence,
-  EvidencesSuspects,
-  FinancialInvest,
-  ForensicInvest,
-  Indictment,
-=======
 import { Victim } from "@/modules/victims/entities/victim.entity";
 import { VictimInterview } from "@/modules/victims_interviews/entities/victim_interview.entity";
 import { WarrantResult } from "@/modules/warrant_results/entities/warrant_result.entity";
@@ -99,20 +50,10 @@ export const entities = [
   ForensicInvest,
   Indictment,
   Inmate, // No relation
->>>>>>> 8bce4497bf846259418229be8f90687e5b0c1b99
   Interview,
   InvestigationPlan,
   MeasureSurvey,
   Permission,
-<<<<<<< HEAD
-  RolesPermissions,
-  PhysicalInvest,
-  Prosecution,
-  Question,
-  RecordInfo,
-  Report,
-  ReportsVictims,
-=======
   PermissionRole,
   PhysicalInvest,
   Prosecution,
@@ -122,23 +63,10 @@ export const entities = [
   Report,
   ReportVictim,
   ReportWitness,
->>>>>>> 8bce4497bf846259418229be8f90687e5b0c1b99
   Role,
   Sentence,
   Suspect,
   Timeline,
-<<<<<<< HEAD
-  UsersCases,
-  User,
-  VictimInterview,
-  Victim,
-  Warrant,
-  WarrantResult,
-  Witness,
-  WitnessesInterviews,
-  ReportsWitnesses,
-  ProsecutionsUsers,
-=======
   User,
   Victim,
   VictimInterview,
@@ -146,5 +74,5 @@ export const entities = [
   Warrant,
   Witness,
   WitnessInterview,
->>>>>>> 8bce4497bf846259418229be8f90687e5b0c1b99
 ];
+export default entities;
